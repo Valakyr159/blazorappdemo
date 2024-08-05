498caed8b4ce3154f3c7ab0dd01001af18b8205a
Javier Morón Mendoza <javiermoron159@hotmail.com>
2024-08-05 00:54:20 -0500

    Added Blazored Integration

--- obj/Debug/net8.0/blazorappdemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazorappdemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazorappdemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7370f48351df95c31db375e5fcfb48c85893c203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44f021e80f3065b9a85ccdf2e212c81f3ca314b")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazorappdemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazorappdemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
